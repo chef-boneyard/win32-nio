2b4f80beb79923f4619c2b382907acf8e37f64f8
Daniel Berger <djberg96@gmail.com>
2012-01-01 09:22:38 -0500

    Added more functions to the Windows::Functions module.

--- lib/win32/windows/functions.rb
+++ lib/win32/windows/functions.rb
@@ -9,9 +9,17 @@ module Windows
 
     attach_function(:CloseHandle, [:long], :int)
     attach_function(:CreateFileA, [:string, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong)
-    attach_function(:GetLastError, [], :ulong)
-    attach_function(:ReadFile, [:ulong, :pointer, :ulong, :pointer, :pointer], :int)
     attach_function(:FormatMessageA, [:ulong, :pointer, :ulong, :ulong, :pointer, :ulong, :pointer], :ulong)
+    attach_function(:GetLastError, [], :ulong)
+    attach_function(:GetSystemInfo, [:pointer], :void)
+    attach_function(:ReadFile, [:ulong, :pointer, :ulong, :pointer, :pointer], :int)
+    attach_function(:ReadFileScatter, [:ulong, :pointer, :ulong, :pointer, :pointer], :int)
+    attach_function(:SleepEx, [:ulong, :int], :ulong)
+    attach_function(:VirtualAlloc, [:pointer, :size_t, :ulong, :ulong], :ulong)
+    attach_function(:VirtualFree, [:ulong, :size_t, :ulong], :int)
+
+    ffi_lib 'msvcrt'
+    attach_function(:memcpy, [:pointer, :pointer, :size_t], :pointer)
 
     FORMAT_MESSAGE_FROM_SYSTEM    = 0x00001000
     FORMAT_MESSAGE_ARGUMENT_ARRAY = 0x00002000
