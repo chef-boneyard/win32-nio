7c01fa520f0282debe6de16282dd5a45613f5c69
Daniel Berger <djberg96@gmail.com>
2012-05-02 13:11:42 -0600

    Changed some function return types.

--- lib/win32/windows/functions.rb
+++ lib/win32/windows/functions.rb
@@ -5,16 +5,16 @@ module Windows
     extend FFI::Library
     ffi_lib :kernel32
 
-    attach_function :CloseHandle, [:long], :int
+    attach_function :CloseHandle, [:long], :bool
     attach_function :CreateFileA, [:string, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
     attach_function :CreateFileW, [:buffer_in, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
     attach_function :GetLastError, [], :ulong
     attach_function :GetSystemInfo, [:pointer], :void
-    attach_function :ReadFile, [:ulong, :buffer_out, :ulong, :pointer, :pointer], :int
-    attach_function :ReadFileScatter, [:ulong, :pointer, :ulong, :pointer, :pointer], :int
+    attach_function :ReadFile, [:ulong, :buffer_out, :ulong, :pointer, :pointer], :bool
+    attach_function :ReadFileScatter, [:ulong, :pointer, :ulong, :pointer, :pointer], :bool
     attach_function :SleepEx, [:ulong, :int], :ulong
     attach_function :VirtualAlloc, [:pointer, :size_t, :ulong, :ulong], :ulong
-    attach_function :VirtualFree, [:ulong, :size_t, :ulong], :int
+    attach_function :VirtualFree, [:ulong, :size_t, :ulong], :bool
 
     ffi_lib :msvcrt
 
