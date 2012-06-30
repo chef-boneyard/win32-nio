bfcb83e045971fd028e0c2ca2ec81ebc4d8a5d62
Daniel Berger <djberg96@gmail.com>
2012-06-30 12:32:11 -0500

    Updated readlines tests.

--- test/test_win32_nio_readlines.rb
+++ test/test_win32_nio_readlines.rb
@@ -3,59 +3,64 @@
 #
 # Test case for the Win32::NIO.readlines method.
 #######################################################################
-require 'rubygems'
-gem 'test-unit'
-require 'test/unit'
+require 'test-unit'
 require 'win32/nio'
 include Win32
 
 class TC_Win32_NIO_Readlines < Test::Unit::TestCase
-   def self.startup
-      @@line = "The quick brown fox jumped over the lazy dog's back"
-      @@file = "readlines_test.txt"
-      @@size = 10
-      File.open(@@file, 'w'){ |fh|
-         1.upto(@@size){ |n|
-            fh.puts @@line + ": #{n}"
-            fh.puts if n % 3 == 0
-         }
+  def self.startup
+    @@line = "The quick brown fox jumped over the lazy dog's back"
+    @@file = "readlines_test.txt"
+    @@size = 10
+
+    File.open(@@file, 'w'){ |fh|
+      1.upto(@@size){ |n|
+        fh.puts @@line + ": #{n}"
+        fh.puts if n % 3 == 0
       }
-   end
+    }
+  end
 
-   def setup
-      @array = nil
-   end
+  def setup
+    @array = nil
+  end
 
-   def test_nio_readlines_basic
-      assert_respond_to(NIO, :readlines)
-   end
+  test "readlines method basic functionality" do
+    assert_respond_to(NIO, :readlines)
+    assert_nothing_raised{ NIO.readlines(@@file) }
+  end
 
-   def test_nio_readlines
-      assert_nothing_raised{ NIO.readlines(@@file) }
-      assert_kind_of(Array, NIO.readlines(@@file))
-      assert_equal(@@size + 3, NIO.readlines(@@file).size)
-      assert_equal(@@line + ': 1', NIO.readlines(@@file).first)
-   end
+  test "readlines returns an array" do
+    assert_kind_of(Array, NIO.readlines(@@file))
+  end
 
-   def test_nio_readlines_with_empty_line_ending
-      assert_nothing_raised{ NIO.readlines(@@file, '') }
-      assert_kind_of(Array, NIO.readlines(@@file, ''))
-      assert_equal(4, NIO.readlines(@@file, '').size)
-   end
+  test "readlines returns an array of the expected size" do
+    assert_equal(@@size + 3, NIO.readlines(@@file).size)
+    assert_equal(@@line + ': 1', NIO.readlines(@@file).first)
+  end
 
-   def test_nio_readlines_expected_errors
-      assert_raise(ArgumentError){ NIO.readlines }
-      assert_raise(ArgumentError){ NIO.readlines(@@file, '', true) }
-   end
+  test "readlines treats an empty second argument as a paragraph separator" do
+    assert_nothing_raised{ NIO.readlines(@@file, '') }
+    assert_kind_of(Array, NIO.readlines(@@file, ''))
+    assert_equal(4, NIO.readlines(@@file, '').size)
+  end
 
-   def teardown
-      @array = nil
-   end
+  test "readlines expects at least one argument" do
+    assert_raise(ArgumentError){ NIO.readlines }
+  end
 
-   def self.shutdown
-      File.delete(@@file) if File.exists?(@@file)
-      @@file = nil
-      @@size = nil
-      @@line = nil
-   end
+  test "readlines accepts a maximum of two arguments" do
+    assert_raise(ArgumentError){ NIO.readlines(@@file, '', true) }
+  end
+
+  def teardown
+    @array = nil
+  end
+
+  def self.shutdown
+    File.delete(@@file) if File.exists?(@@file)
+    @@file = nil
+    @@size = nil
+    @@line = nil
+  end
 end
