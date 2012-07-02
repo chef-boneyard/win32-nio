cce06506dec2678b6e35b2a52f601186e4091a87
Daniel Berger <djberg96@gmail.com>
2012-07-02 09:35:27 -0600

    Added ReadFileEx and GetOverlappedResult to function list.

--- lib/win32/windows/functions.rb
+++ lib/win32/windows/functions.rb
@@ -8,8 +8,10 @@ module Windows
     attach_function :CloseHandle, [:long], :bool
     attach_function :CreateFileA, [:string, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
     attach_function :CreateFileW, [:buffer_in, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
+    attach_function :GetOverlappedResult, [:ulong, :pointer, :pointer, :bool], :bool
     attach_function :GetSystemInfo, [:pointer], :void
     attach_function :ReadFile, [:ulong, :buffer_out, :ulong, :pointer, :pointer], :bool
+    attach_function :ReadFileEx, [:ulong, :buffer_out, :ulong, :pointer, :pointer], :bool
     attach_function :ReadFileScatter, [:ulong, :pointer, :ulong, :pointer, :pointer], :bool
     attach_function :SleepEx, [:ulong, :bool], :ulong
     attach_function :VirtualAlloc, [:pointer, :size_t, :ulong, :ulong], :ulong
