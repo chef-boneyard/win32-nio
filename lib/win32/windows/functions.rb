6d4b21bfe1f80f160d0644eeb75e8170f563a619
Daniel Berger <djberg96@gmail.com>
2012-07-09 16:07:37 -0600

    Updated ReadFileEx declaration and added a callback prototype for it, and set :stdcall convention.

--- lib/win32/windows/functions.rb
+++ lib/win32/windows/functions.rb
@@ -4,6 +4,7 @@ module Windows
   module Functions
     extend FFI::Library
     ffi_lib :kernel32
+    ffi_convention :stdcall
 
     attach_function :CloseHandle, [:long], :bool
     attach_function :CreateFileA, [:string, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
@@ -11,10 +12,12 @@ module Windows
     attach_function :GetOverlappedResult, [:ulong, :pointer, :pointer, :bool], :bool
     attach_function :GetSystemInfo, [:pointer], :void
     attach_function :ReadFile, [:ulong, :buffer_out, :ulong, :pointer, :pointer], :bool
-    attach_function :ReadFileEx, [:ulong, :buffer_out, :ulong, :pointer, :pointer], :bool
     attach_function :ReadFileScatter, [:ulong, :pointer, :ulong, :pointer, :pointer], :bool
     attach_function :SleepEx, [:ulong, :bool], :ulong
     attach_function :VirtualAlloc, [:pointer, :size_t, :ulong, :ulong], :ulong
     attach_function :VirtualFree, [:ulong, :size_t, :ulong], :bool
+
+    callback :completion_function, [:ulong, :ulong, :pointer], :void
+    attach_function :ReadFileEx, [:ulong, :buffer_out, :ulong, :pointer, :completion_function], :bool
   end
 end
