19307445bb6f4b140f9e74390effd9da4b3fbc38
Daniel Berger <djberg96@gmail.com>
2013-04-10 06:27:56 -0600

    Updated HANDLE type and added some typedefs.

--- lib/win32/windows/functions.rb
+++ lib/win32/windows/functions.rb
@@ -3,21 +3,25 @@ require 'ffi'
 module Windows
   module Functions
     extend FFI::Library
+    typedef :ulong, :dword
+    typedef :uintptr_t, :handle
+    typedef :pointer, :ptr
+
     ffi_lib :kernel32
     ffi_convention :stdcall
 
-    attach_function :CloseHandle, [:long], :bool
-    attach_function :CreateFileA, [:string, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
-    attach_function :CreateFileW, [:buffer_in, :ulong, :ulong, :pointer, :ulong, :ulong, :ulong], :ulong
-    attach_function :GetOverlappedResult, [:ulong, :pointer, :pointer, :bool], :bool
-    attach_function :GetSystemInfo, [:pointer], :void
-    attach_function :ReadFile, [:ulong, :buffer_out, :ulong, :pointer, :pointer], :bool
-    attach_function :ReadFileScatter, [:ulong, :pointer, :ulong, :pointer, :pointer], :bool
-    attach_function :SleepEx, [:ulong, :bool], :ulong
-    attach_function :VirtualAlloc, [:pointer, :size_t, :ulong, :ulong], :ulong
-    attach_function :VirtualFree, [:ulong, :size_t, :ulong], :bool
+    attach_function :CloseHandle, [:handle], :bool
+    attach_function :CreateFileA, [:string, :dword, :dword, :ptr, :dword, :dword, :handle], :handle
+    attach_function :CreateFileW, [:buffer_in, :dword, :dword, :ptr, :dword, :dword, :handle], :handle
+    attach_function :GetOverlappedResult, [:handle, :ptr, :ptr, :bool], :bool
+    attach_function :GetSystemInfo, [:ptr], :void
+    attach_function :ReadFile, [:handle, :buffer_out, :dword, :ptr, :ptr], :bool
+    attach_function :ReadFileScatter, [:handle, :ptr, :dword, :ptr, :ptr], :bool
+    attach_function :SleepEx, [:dword, :bool], :dword
+    attach_function :VirtualAlloc, [:ptr, :size_t, :dword, :dword], :dword
+    attach_function :VirtualFree, [:dword, :size_t, :dword], :bool
 
-    callback :completion_function, [:ulong, :ulong, :pointer], :void
-    attach_function :ReadFileEx, [:ulong, :buffer_out, :ulong, :pointer, :completion_function], :bool
+    callback :completion_function, [:dword, :dword, :ptr], :void
+    attach_function :ReadFileEx, [:handle, :buffer_out, :dword, :ptr, :completion_function], :bool
   end
 end
