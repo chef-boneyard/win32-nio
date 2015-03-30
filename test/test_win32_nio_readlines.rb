55d6928d41510a4c9e021d5393719e7d287257a0
Daniel Berger <djberg96@gmail.com>
2015-03-30 06:26:54 -0600

    Updated to handle possible event objects as 3rd argument.

--- test/test_win32_nio_readlines.rb
+++ test/test_win32_nio_readlines.rb
@@ -5,6 +5,7 @@
 #######################################################################
 require 'test-unit'
 require 'win32/nio'
+require 'win32/event'
 include Win32
 
 class TC_Win32_NIO_Readlines < Test::Unit::TestCase
@@ -23,6 +24,7 @@ class TC_Win32_NIO_Readlines < Test::Unit::TestCase
 
   def setup
     @array = nil
+    @event = Win32::Event.new
   end
 
   test "readlines method basic functionality" do
@@ -45,20 +47,27 @@ class TC_Win32_NIO_Readlines < Test::Unit::TestCase
     assert_equal(4, NIO.readlines(@@file, '').size)
   end
 
+  test "readlines accepts an event object" do
+    assert_false(@event.signaled?)
+    assert_nothing_raised{ NIO.readlines(@@file, nil, @event) }
+    assert_true(@event.signaled?)
+  end
+
   test "readlines expects at least one argument" do
     assert_raise(ArgumentError){ NIO.readlines }
   end
 
-  test "readlines accepts a maximum of two arguments" do
-    assert_raise(ArgumentError){ NIO.readlines(@@file, '', true) }
+  test "readlines accepts a maximum of three arguments" do
+    assert_raise(ArgumentError){ NIO.readlines(@@file, '', @event, true) }
   end
 
   def teardown
     @array = nil
+    @event = nil
   end
 
   def self.shutdown
-    File.delete(@@file) if File.exists?(@@file)
+    File.delete(@@file) if File.exist?(@@file)
     @@file = nil
     @@size = nil
     @@line = nil
